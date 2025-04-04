065f6859c7ad82e90caa61c80dc78f73be31b925
karthik123-hash <karthikdputhran@gmail.com>
2025-04-04 10:30:59 +0530

    Initial commit

---
+++ demo-proj1/src/main/java/com/example/demo_proj1/DemoProj1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_proj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProj1Application.class, args);
+	}
+
+}
---
+++ demo-proj1/src/test/java/com/example/demo_proj1/DemoProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_proj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
